5324f158d7884ef08992bb878058857b727357c4
Justin Kenyon <kenyonj@gmail.com>
2014-03-03 12:11:57 -0500

    finish OOP refactor

--- day01/flashcards.rb
+++ day01/flashcards.rb
@@ -5,13 +5,34 @@ class Card
     @back = back
   end
 
+  def play
+    puts "Guess the answer to #{front}"
+    correct_guess = false
+
+    3.times do
+      print "> "
+      guess = gets.strip
+
+      if correct_guess?(guess)
+        correct_guess = true
+        break
+      end
+    end
+
+    if correct_guess
+      puts "You are correct!"
+    else
+      puts "Sorry, it was actually #{back}."
+    end
+  end
+
   def correct_guess?(guess)
-    guess.downcase == @back.downcase
+    guess.downcase == back.downcase
   end
 end
 
 class Deck
-  attr_reader :cards
+  attr_accessor :cards
   def initialize(filename)
     @cards = []
     file = File.open(filename, "r")
@@ -19,7 +40,13 @@ class Deck
     card_count.times do
       front = file.gets.strip
       back = file.gets.strip
-      @cards << Card.new(front, back)
+      cards << Card.new(front, back)
+    end
+  end
+
+  def play
+    cards.each do |card|
+      card.play
     end
   end
 end
@@ -28,29 +55,17 @@ class Game
   def initialize(deck)
     @deck = deck
   end
+
   def play
+    print_welcome_message
+    @deck.play
+  end
+
+  private
+
+  def print_welcome_message
     puts "Welcome to my flashcard app!"
     puts "-" * 28
-    @deck.cards.each do |card|
-      puts "Guess the answer to #{card.front}"
-      correct_guess = false
-
-      3.times do
-        print "> "
-        guess = gets.strip
-
-        if card.correct_guess?(guess)
-          correct_guess = true
-          break
-        end
-      end
-
-      if correct_guess
-        puts "You are correct!"
-      else
-        puts "Sorry, it was actually #{card.back}."
-      end
-    end
   end
 end
 
